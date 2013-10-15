1305ff0714089d957756b7641dd405e2d1e9c45e
Apprentice <apprentice@dbc-23.local>
2013-10-15 12:20:21 -0700

    progress to postyness

--- post-it.js
+++ post-it.js
@@ -1,21 +1,24 @@
 var Board = function( selector ) {
   // Your board related code goes here
-  
+
   // Use $elem to access the DOM element for this board
   var $elem = $( selector );
-  
+
   function initialize() {
-    // What needs to happen when this object is created?
+    $elem.on("click", function() {
+      new PostIt()
+    })
   };
 
   initialize();
 };
 
 var PostIt = function() {
-  // Your post-it related code goes here
-};
+  $(".post-it:first").clone().removeClass('hidden').appendTo("#board");
+}
+
 
 $(function() {
   // This code will run when the DOM has finished loading
-  Board.new('#board');
+  new Board('#board');
 });
